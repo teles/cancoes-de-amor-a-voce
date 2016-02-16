10812b763fb345facd84bf6055044403f4200156
teles <josetelesmaciel@gmail.com>
2016-02-16 20:49:56 -0200

    corrigindo typo no travis

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -4,5 +4,6 @@ rm -rf livro || exit 0;
 gitbook build . livro;
 git add .
 git commit -m "Atualizando gh-pages via travis-ci"
+
 # DEPLOY_REPO
-git push --force --quiet "https://$GH_TOKEN@$github.com/teles/cancoes-de-amor-a-voce.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://$GH_TOKEN@github.com/teles/cancoes-de-amor-a-voce.git" master:gh-pages > /dev/null 2>&1
