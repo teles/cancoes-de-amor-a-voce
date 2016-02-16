42c966a511d52d1b4ba18e1bb02103d78358a816
teles <josetelesmaciel@gmail.com>
2016-02-16 20:55:32 -0200

    lascando after script nesse trem

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -4,6 +4,3 @@ rm -rf livro || exit 0;
 gitbook build . livro;
 git add .
 git commit -m "Atualizando gh-pages via travis-ci"
-
-# DEPLOY_REPO
-git push --force --quiet "https://$GH_TOKEN@github.com/teles/cancoes-de-amor-a-voce.git" master:gh-pages > /dev/null 2>&1
