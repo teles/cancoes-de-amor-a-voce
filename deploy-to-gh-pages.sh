5d4ac5841a93ed317cc0adc0fd7deb0ea49a80ee
teles <josetelesmaciel@gmail.com>
2016-02-16 20:33:27 -0200

    concertando erro de typo no deploy

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -3,5 +3,5 @@
 rm -rf livro || exit 0;
 gitbook build . livro;
 git add .
-git commmit -m "Atualizando gh-pages via travis-ci"
+git commit -m "Atualizando gh-pages via travis-ci"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
